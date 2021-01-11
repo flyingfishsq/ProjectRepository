8e4024b1f1106d3f32ffd18aa046346f5075c723
Qi_Computer <flyingifshsq@aliyun.com>
2021-01-11 23:18:01 +0800

    控件的状态

--- F_Demo_1/lib/demo/state/state_management_demo.dart
+++ F_Demo_1/lib/demo/state/state_management_demo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 //不包含可以变化的状态（数据）
 //这里定义了一个可变化的变量count，这个类报出了提示，在实际调试中，点击按钮，输出会有显示，但是控件中的count不会变化
@@ -29,53 +30,90 @@ import 'package:flutter/material.dart';
 
 //这个StatefulWidget本身也是不可改变的
 //将要变化的数据，得放在State类里，且数据的变化一定要写在setState方法里，否则不起作用
-class StateManagementDemo extends StatefulWidget {
-  StateManagementDemo({Key key}) : super(key: key);
+// class StateManagementDemo extends StatefulWidget {
+//   StateManagementDemo({Key key}) : super(key: key);
 
-  @override
-  _StateManagementDemoState createState() => _StateManagementDemoState();
-}
+//   @override
+//   _StateManagementDemoState createState() => _StateManagementDemoState();
+// }
 
-class _StateManagementDemoState extends State<StateManagementDemo> {
-  int _count = 0;
-  void _increaseCount() {
-    setState(() {
-      _count += 1;
-    });
-    print(_count);
-  }
+// class _StateManagementDemoState extends State<StateManagementDemo> {
+//   int _count = 0;
+//   void _increaseCount() {
+//     setState(() {
+//       _count += 1;
+//     });
+//     print(_count);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // return Scaffold(
+//     //   appBar: AppBar(
+//     //     title: Text('StateManagementDemo'),
+//     //   ),
+//     //   // body: Center(
+//     //   //   child: Chip(
+//     //   //     label: Text('$_count'),
+//     //   //   ),
+//     //   // ),
+
+//     //   // body: Counter(_count, _increaseCount),
+
+//     //   body: CountWrapper(_count, _increaseCount),
+
+//     //   floatingActionButton: FloatingActionButton(
+//     //     child: Icon(Icons.add),
+//     //     // onPressed: () {
+//     //     // setState(() {
+//     //     //   _count += 1;
+//     //     // });
+//     //     // print(_count);
+//     //     // },
+//     //     onPressed: _increaseCount,
+//     //   ),
+//     // );
+
+//     return CounterProvider(
+//       count: _count,
+//       increaseCount: _increaseCount,
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: Text('StateManagementDemo'),
+//         ),
+//         // body: Center(
+//         //   child: Chip(
+//         //     label: Text('$_count'),
+//         //   ),
+//         // ),
+
+//         // body: Counter(_count, _increaseCount),
+
+//         body: CountWrapper(),
+
+//         floatingActionButton: FloatingActionButton(
+//           child: Icon(Icons.add),
+//           // onPressed: () {
+//           // setState(() {
+//           //   _count += 1;
+//           // });
+//           // print(_count);
+//           // },
+//           onPressed: _increaseCount,
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+//使用ScopedModel将小部件改造成一个StatelessWidget
+class StateManagementDemo extends StatelessWidget {
+  const StateManagementDemo({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // return Scaffold(
-    //   appBar: AppBar(
-    //     title: Text('StateManagementDemo'),
-    //   ),
-    //   // body: Center(
-    //   //   child: Chip(
-    //   //     label: Text('$_count'),
-    //   //   ),
-    //   // ),
-
-    //   // body: Counter(_count, _increaseCount),
-
-    //   body: CountWrapper(_count, _increaseCount),
-
-    //   floatingActionButton: FloatingActionButton(
-    //     child: Icon(Icons.add),
-    //     // onPressed: () {
-    //     // setState(() {
-    //     //   _count += 1;
-    //     // });
-    //     // print(_count);
-    //     // },
-    //     onPressed: _increaseCount,
-    //   ),
-    // );
-
-    return CounterProvider(
-      count: _count,
-      increaseCount: _increaseCount,
+    return ScopedModel(
+      model: CounterModel(),
       child: Scaffold(
         appBar: AppBar(
           title: Text('StateManagementDemo'),
@@ -90,15 +128,19 @@ class _StateManagementDemoState extends State<StateManagementDemo> {
 
         body: CountWrapper(),
 
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          // onPressed: () {
-          // setState(() {
-          //   _count += 1;
-          // });
-          // print(_count);
-          // },
-          onPressed: _increaseCount,
+        floatingActionButton: ScopedModelDescendant<CounterModel>(
+          //是否需要重建FloatingActionButton小部件
+          rebuildOnChange: false,
+          builder: (context, _, model) => FloatingActionButton(
+            child: Icon(Icons.add),
+            // onPressed: () {
+            // setState(() {
+            //   _count += 1;
+            // });
+            // print(_count);
+            // },
+            onPressed: model.increaseCount,
+          ),
         ),
       ),
     );
@@ -131,16 +173,22 @@ class Counter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //在需要使用的地方去定义
-    final int count = CounterProvider.of(context).count;
-    final VoidCallback increaseCount =
-        CounterProvider.of(context).increaseCount;
+    // //在需要使用的地方去定义
+    // final int count = CounterProvider.of(context).count;
+    // final VoidCallback increaseCount =
+    //     CounterProvider.of(context).increaseCount;
 
-    return ActionChip(
-      label: Text('$count'),
-      //从父辈那里传递个回调
-      onPressed: increaseCount,
-    );
+    // return ActionChip(
+    //   label: Text('$count'),
+    //   //从父辈那里传递个回调
+    //   onPressed: increaseCount,
+    // );
+
+    //使用ScopedModel时，对Counter的改写
+
+    return ScopedModelDescendant<CounterModel>(
+        builder: (context, _, model) => ActionChip(
+            label: Text('${model.count}'), onPressed: model.increaseCount));
   }
 }
 
@@ -161,5 +209,17 @@ class CounterProvider extends InheritedWidget {
   @override
   bool updateShouldNotify(CounterProvider oldWidget) {
     return true;
+  }
+}
+
+//使用ScopedModel实现数据的传递
+//使用这个model的小部件会监听model的变化，执行到notifyListeners时小部件会重建
+class CounterModel extends Model {
+  int _count = 0;
+  int get count => _count;
+
+  void increaseCount() {
+    _count += 1;
+    notifyListeners();
   }
 }
