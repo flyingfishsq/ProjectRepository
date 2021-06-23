3bf3bee3249c8f86acb36b9fc6837dede31f458e
flyingfishsq@aliyun.com <flyingfishsq@aliyun.com>
2021-06-23 19:29:18 +0800

    listview的item也可以是个listview

--- flutter_base_bigd/lib/main.dart
+++ flutter_base_bigd/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,109 +6,208 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('ListTile'),
+        ),
+        body: HomeContent(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class HomeContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      padding: EdgeInsets.all(8),
+      children: [
+
+        /***设置为scrollDirection: Axis.horizontal，要明确item的宽度***/
+        Container(
+          width: MediaQuery.of(context).size.width,
+          child: ListTile(
+            leading: Image.asset('images/type_channellgs.png'),
+            trailing: Icon(
+              Icons.icecream,
+              color: Colors.green,
+              size: 42,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            title: Text(
+              '1.华南强降雨持续“发力” 华北东北雷阵雨勤“打卡”',
+              style: TextStyle(fontSize: 18, color: Colors.black),
             ),
-          ],
+            subtitle: Text(
+              '预计今明两天，江南、华南等地仍将有较强降水，尤其是广东南部、云南东部等地局地或有大暴雨现身，并伴有强对流天气。此外，受弱冷空气影响，华北、东北等地多阵雨或雷阵雨。',
+              style: TextStyle(fontSize: 12, color: Colors.grey),
+            ),
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        Container(
+          width: MediaQuery.of(context).size.width,
+          child: ListTile(
+            leading: Image.asset('images/type_channellgs.png'),
+            trailing: Icon(
+              Icons.icecream,
+              color: Colors.green,
+              size: 42,
+            ),
+            title: Text(
+              '1.华南强降雨持续“发力” 华北东北雷阵雨勤“打卡”',
+              style: TextStyle(fontSize: 18, color: Colors.black),
+            ),
+            subtitle: Text(
+              '预计今明两天，江南、华南等地仍将有较强降水，尤其是广东南部、云南东部等地局地或有大暴雨现身，并伴有强对流天气。此外，受弱冷空气影响，华北、东北等地多阵雨或雷阵雨。',
+              style: TextStyle(fontSize: 12, color: Colors.grey),
+            ),
+          ),
+        ),
+        /***设置为scrollDirection: Axis.horizontal，要明确item的宽度***/
+
+        // ListTile(
+        //   leading: Image.asset('images/type_channellgs.png'),
+        //   trailing: Icon(
+        //     Icons.icecream,
+        //     color: Colors.green,
+        //     size: 42,
+        //   ),
+        //   title: Text(
+        //     '1.华南强降雨持续“发力” 华北东北雷阵雨勤“打卡”',
+        //     style: TextStyle(fontSize: 18, color: Colors.black),
+        //   ),
+        //   subtitle: Text(
+        //     '预计今明两天，江南、华南等地仍将有较强降水，尤其是广东南部、云南东部等地局地或有大暴雨现身，并伴有强对流天气。此外，受弱冷空气影响，华北、东北等地多阵雨或雷阵雨。',
+        //     style: TextStyle(fontSize: 12, color: Colors.grey),
+        //   ),
+        // ),
+        // ListTile(
+        //   leading: Image.asset('images/type_channellgs.png'),
+        //   trailing: Icon(
+        //     Icons.star,
+        //     color: Colors.orange,
+        //     size: 42,
+        //   ),
+        //   title: Text(
+        //     '2.华南强降雨持续“发力” 华北东北雷阵雨勤“打卡”',
+        //     style: TextStyle(fontSize: 18, color: Colors.black),
+        //   ),
+        //   subtitle: Text(
+        //     '预计今明两天，江南、华南等地仍将有较强降水，尤其是广东南部、云南东部等地局地或有大暴雨现身，并伴有强对流天气。此外，受弱冷空气影响，华北、东北等地多阵雨或雷阵雨。',
+        //     style: TextStyle(fontSize: 12, color: Colors.grey),
+        //   ),
+        // ),
+        // ListTile(
+        //   leading: Container(
+        //     width: 45,
+        //     height: 45,
+        //     child: CircleAvatar(
+        //       backgroundImage: NetworkImage(
+        //           'https://t7.baidu.com/it/u=1595072465,3644073269&fm=193&f=GIF'),
+        //       radius: 20,
+        //     ),
+        //   ),
+        //   trailing: Container(
+        //     width: 80,
+        //     height: 50,
+        //     child: Image.network(
+        //       'https://t7.baidu.com/it/u=1951548898,3927145&fm=193&f=GIF',
+        //       fit: BoxFit.cover,
+        //     ),
+        //   ),
+        //   title: Text(
+        //     '3.华南强降雨持续“发力” 华北东北雷阵雨勤“打卡”',
+        //     style: TextStyle(fontSize: 18, color: Colors.black),
+        //   ),
+        //   subtitle: Text(
+        //     '预计今明两天，江南、华南等地仍将有较强降水，尤其是广东南部、云南东部等地局地或有大暴雨现身，并伴有强对流天气。此外，受弱冷空气影响，华北、东北等地多阵雨或雷阵雨。',
+        //     style: TextStyle(fontSize: 12, color: Colors.grey),
+        //   ),
+        // ),
+        // ListTile(
+        //   leading: Image.asset('images/type_channellgs.png'),
+        //   trailing: Image.asset('images/type_cruise.png'),
+        //   title: Text(
+        //     '4.华南强降雨持续“发力” 华北东北雷阵雨勤“打卡”',
+        //     style: TextStyle(fontSize: 18, color: Colors.black),
+        //   ),
+        //   subtitle: Text(
+        //     '预计今明两天，江南、华南等地仍将有较强降水，尤其是广东南部、云南东部等地局地或有大暴雨现身，并伴有强对流天气。此外，受弱冷空气影响，华北、东北等地多阵雨或雷阵雨。',
+        //     style: TextStyle(fontSize: 12, color: Colors.grey),
+        //   ),
+        // ),
+        // ListTile(
+        //   leading: Image.asset('images/type_channellgs.png'),
+        //   trailing: Image.asset('images/type_cruise.png'),
+        //   title: Text(
+        //     '5.华南强降雨持续“发力” 华北东北雷阵雨勤“打卡”',
+        //     style: TextStyle(fontSize: 18, color: Colors.black),
+        //   ),
+        //   subtitle: Text(
+        //     '预计今明两天，江南、华南等地仍将有较强降水，尤其是广东南部、云南东部等地局地或有大暴雨现身，并伴有强对流天气。此外，受弱冷空气影响，华北、东北等地多阵雨或雷阵雨。',
+        //     style: TextStyle(fontSize: 12, color: Colors.grey),
+        //   ),
+        // ),
+        // ListTile(
+        //   leading: Image.asset('images/type_channellgs.png'),
+        //   trailing: Image.asset('images/type_cruise.png'),
+        //   title: Text(
+        //     '6.华南强降雨持续“发力” 华北东北雷阵雨勤“打卡”',
+        //     style: TextStyle(fontSize: 18, color: Colors.black),
+        //   ),
+        //   subtitle: Text(
+        //     '预计今明两天，江南、华南等地仍将有较强降水，尤其是广东南部、云南东部等地局地或有大暴雨现身，并伴有强对流天气。此外，受弱冷空气影响，华北、东北等地多阵雨或雷阵雨。',
+        //     style: TextStyle(fontSize: 12, color: Colors.grey),
+        //   ),
+        // ),
+        // ListTile(
+        //   leading: Image.asset('images/type_channellgs.png'),
+        //   trailing: Image.asset('images/type_cruise.png'),
+        //   title: Text(
+        //     '7.华南强降雨持续“发力” 华北东北雷阵雨勤“打卡”',
+        //     style: TextStyle(fontSize: 18, color: Colors.black),
+        //   ),
+        //   subtitle: Text(
+        //     '预计今明两天，江南、华南等地仍将有较强降水，尤其是广东南部、云南东部等地局地或有大暴雨现身，并伴有强对流天气。此外，受弱冷空气影响，华北、东北等地多阵雨或雷阵雨。',
+        //     style: TextStyle(fontSize: 12, color: Colors.grey),
+        //   ),
+        // ),
+        // Container(
+        //   decoration: BoxDecoration(color: Colors.limeAccent),
+        //   padding: EdgeInsets.all(4),
+        //   child: ListTile(
+        //     leading: Image.asset('images/type_channellgs.png'),
+        //     trailing: Image.asset('images/type_cruise.png'),
+        //     title: Text(
+        //       '8.华南强降雨持续“发力” 华北东北雷阵雨勤“打卡”',
+        //       style: TextStyle(fontSize: 18, color: Colors.black),
+        //     ),
+        //     subtitle: Text(
+        //       '预计今明两天，江南、华南等地仍将有较强降水，尤其是广东南部、云南东部等地局地或有大暴雨现身，并伴有强对流天气。此外，受弱冷空气影响，华北、东北等地多阵雨或雷阵雨。',
+        //       style: TextStyle(fontSize: 12, color: Colors.grey),
+        //     ),
+        //   ),
+        // ),
+        // Container(
+        //   margin: EdgeInsets.all(4),
+        //   decoration: BoxDecoration(
+        //       color: Colors.orange,
+        //       borderRadius: BorderRadius.all(Radius.circular(10))),
+        //   width: MediaQuery.of(context).size.width,
+        //   height: 120,
+        //   child: Image.network(
+        //     'https://pics4.baidu.com/feed/d50735fae6cd7b89f35651d1465c92a1d9330e2c.jpeg?token=df1cd7bb57d491075a0cbc573199f3dc',
+        //     fit: BoxFit.contain,
+        //   ),
+        // ),
+      ],
     );
   }
 }
