f858ff1c67b818bf1de114e3e40e6d065a82e80c
flyingfishsq@aliyun.com <flyingfishsq@aliyun.com>
2021-06-23 11:29:40 +0800

    搜索的完成

--- Flutter_Trip/flutter_trip_app/lib/pages/search_page.dart
+++ Flutter_Trip/flutter_trip_app/lib/pages/search_page.dart
@@ -8,6 +8,22 @@ import 'package:flutter_trip_app/widget/web_view.dart';
 const String URL =
     'https://m.ctrip.com/restapi/h5api/globalsearch/search?source=mobileweb&action=mobileweb&keyword=';
 
+const TYPES = [
+  'channelgroup',
+  'channellgs',
+  'channelplane',
+  'channeltrain',
+  'cruise',
+  'district',
+  'food',
+  'hotel',
+  'huodong',
+  'shop',
+  'sight',
+  'ticket',
+  'travelgroup'
+];
+
 class SearchPage extends StatefulWidget {
   final bool hideLeft;
   final String searchUrl;
@@ -130,16 +146,25 @@ class _SearchPageState extends State<SearchPage> {
             border: Border(bottom: BorderSide(width: 0.3, color: Colors.grey))),
         child: Row(
           children: [
+            Container(
+              margin: EdgeInsets.all(2),
+              child: Image(
+                height: 32,
+                width: 32,
+                image: AssetImage(_typeImage(item.type)),
+              ),
+            ),
             Column(
               children: [
                 Container(
                   width: 300,
                   //字符串为空对象的时候返回空字符串
-                  child: Text('${item.word} ${item.districtname??''} ${item.zonename??''}'),
+                  child: _title(item),
                 ),
                 Container(
                   width: 300,
-                  child: Text('${item.price??''} ${item.type??''}'),
+                  margin: EdgeInsets.only(top: 4),
+                  child: _subTitle(item),
                 ),
               ],
             )
@@ -147,5 +172,71 @@ class _SearchPageState extends State<SearchPage> {
         ),
       ),
     );
+  }
+
+  //根据类型匹配到文件路径
+  String _typeImage(String type) {
+    if (type == null) return 'images/type_travelgroup.png';
+
+    String path = 'travelgroup';
+    for (final val in TYPES) {
+      if (type.contains(val)) {
+        path = val;
+        break;
+      }
+    }
+    return 'images/type_$path.png';
+  }
+
+  _title(SearchItem item) {
+    if (item == null) return null;
+
+    //TextSpan是用来显示富文本的一个辅助类
+    List<TextSpan> spans = [];
+    spans.addAll(_keywordTextSpans(item.word, searchModel.keyword));
+
+    spans.add(TextSpan(
+        text: '  ' + (item.districtname ?? '') + (item.zonename ?? ''),
+        style: TextStyle(fontSize: 16, color: Colors.grey)));
+    return RichText(
+      text: TextSpan(children: spans),
+    );
+  }
+
+  _subTitle(SearchItem item) {
+    return RichText(
+      text: TextSpan(children: [
+        TextSpan(
+            text: item.price ?? '',
+            style: TextStyle(fontSize: 16, color: Colors.orange)),
+        TextSpan(
+            text: '  '+(item.star ?? ''),
+            style: TextStyle(fontSize: 14, color: Colors.grey)),
+      ]),
+    );
+  }
+
+  //以搜索关键字为断点，把字符串拆分
+  Iterable<TextSpan> _keywordTextSpans(String word, String keyword) {
+    List<TextSpan> spans = [];
+    if (word == null || word.length == 0) return spans;
+
+    List<String> arr = word.split(keyword);
+    TextStyle normalStyle = TextStyle(fontSize: 16, color: Colors.black87);
+    TextStyle keywordStyle = TextStyle(fontSize: 16, color: Colors.orange);
+
+    // wordwoc split w [,ord,oc]
+    for (int i = 0; i < arr.length; i++) {
+      //根据奇数位还是偶数位
+      if ((i + 1) % 2 == 0) {
+        spans.add(TextSpan(text: keyword, style: keywordStyle));
+      }
+      String val = arr[i];
+      //不是空字符串
+      if (val != null && val.length > 0) {
+        spans.add(TextSpan(text: val, style: normalStyle));
+      }
+    }
+    return spans;
   }
 }
